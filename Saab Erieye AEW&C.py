825487b083fe28ec7b85ac5a776d589a21c341e9
Ahmed Sheraz <ahmedsheraz1@gmail.com>
2022-12-14 10:07:21 +0500

    Create Saab Erieye AEW&C.py

---
+++ Saab Erieye AEW&C.py
@@ -0,0 +1,5 @@
+Python 3.10.2 (v3.10.2:a58ebcc701, Jan 13 2022, 14:50:16) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+from hashlib import blake2b
+blake2b(b'Saab Erieye').hexdigest()
+'00e84292f9ca6bf925e27837fbc1333ba1eb3b17e250ec49eb3846d30ad2fc47e3df6198b2db5d5693262280b3df9e187ddf5d6a2a16f99b655b86d916d39a6e'
